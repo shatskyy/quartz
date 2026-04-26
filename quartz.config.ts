c74a9c3efcb631c2a304a5cd969dcc4c96ffef64
shatskyy <139797894+shatskyy@users.noreply.github.com>
2026-04-26 18:48:20 -0400

    Update baseUrl in quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "quartz-zeta-kohl.vercel.app",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
